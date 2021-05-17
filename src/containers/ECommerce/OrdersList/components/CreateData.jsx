bfe06f6abba1b487fe900e19b9da9b45fa8b6ae7
Aryan Phuyal <phuyalrn2@gmail.com>
2021-05-17 15:03:16 +0545

    initial-commit

--- src/containers/ECommerce/OrdersList/components/CreateData.jsx
+++ src/containers/ECommerce/OrdersList/components/CreateData.jsx
@@ -1,143 +1,137 @@
-import React, { useMemo, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getorderstart } from '../../../../redux/actions/orderActions';
-import { Link } from 'react-router-dom';
+import React, { useMemo, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getorderstart } from "../../../../redux/actions/orderActions";
+import { Link } from "react-router-dom";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const MoneyFormatter = ({ value }) => (
-  <div>
-    ${value}
-  </div>
-);
-const ActionFormatter = val => (
-  [
-  <Link to={`./orderdetails/${val}`}  className="btn btn-outline-primary btn-sm"><span class="lnr lnr-eye"></span></Link>
-  ]
-  );
-  ActionFormatter.propTypes = {
-    value: PropTypes.string.isRequired,
-  };
+const MoneyFormatter = ({ value }) => <div>${value}</div>;
+const ActionFormatter = (val) => [
+  <Link to={`./orderdetails/${val}`} className="btn btn-outline-primary btn-sm">
+    <span class="lnr lnr-eye"></span>
+  </Link>,
+];
+ActionFormatter.propTypes = {
+  value: PropTypes.string.isRequired,
+};
 MoneyFormatter.propTypes = {
   value: PropTypes.string.isRequired,
 };
 
-const StatusFormatter = ({ value }) => (
-  value === 'Enabled' ? <span className="badge badge-success">Enabled</span>
-  : <span className="badge badge-secondary">Disabled</span>
+const StatusFormatter = ({ value }) =>
+  value === "Enabled" ? (
+    <span className="badge badge-success">Enabled</span>
+  ) : (
+    <span className="badge badge-secondary">Disabled</span>
   );
-  
-  StatusFormatter.propTypes = {
-    value: PropTypes.string.isRequired,
-  };
-  
-  let data = [];
-  const CreateDataOrderListTable = () => {
+
+StatusFormatter.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+let data = [];
+const CreateDataOrderListTable = () => {
   const dispatch = useDispatch();
 
-  var token = localStorage.getItem('token');
-  // update this line 
-  const {done,orders} = useSelector(state=>state.orders);
-   
+  var token = localStorage.getItem("token");
+  // update this line
+  const { done, orders } = useSelector((state) => state.orders);
+
   useEffect(() => {
-    if(!done && orders.length === 0 ){
-
-      // api call  
-        dispatch(getorderstart(token))
+    if (!done && orders.length === 0) {
+      // api call
+      dispatch(getorderstart(token));
     }
-   
+  });
 
-  })
-
-  if(done && orders.length !== 0){
-      
+  if (done && orders.length !== 0) {
     data = [];
-  let coun =1;
-    orders.map(order => {
-        // console.log(item)
-        data.push({
-          id: coun,
-          date: order.orderedDate,
-          name: order.billingDetails.name,
-          price: order.totalCost.toString(),
-          address: order.billingDetails.address,
-          phone: order.billingDetails.phone.toString(),
-          quantity: order.totalProducts.toString(),
-          status: StatusFormatter(order.status),
-          actionn: [
-            (ActionFormatter(order._id)),
-          ],
-        });
-        coun++;
-      })
-    
-    }
+    let coun = 1;
+    orders.map((order) => {
+      console.log("-=====");
+      console.log(order);
+      console.log("-=====");
+
+      data.push({
+        id: coun,
+        date: order.orderedDate,
+        name: order.shippingAddress ? order.shippingAddress.name : "",
+        price: order.totalCost.toString(),
+        address: order.shippingAddress.address,
+        phone: order.shippingAddress.phone.toString(),
+        quantity: order.totalProducts.toString(),
+        status: StatusFormatter(order.status),
+        actionn: [ActionFormatter(order._id)],
+      });
+      coun++;
+    });
+  }
 
   const columns = useMemo(
     () => [
       {
-        accessor: 'id',
-        Header: 'ID',
+        accessor: "id",
+        Header: "ID",
         width: 80,
         disableGlobalFilter: true,
       },
       {
-        accessor: 'date',
-        Header: 'Date',
+        accessor: "date",
+        Header: "Date",
         disableSortBy: true,
         disableGlobalFilter: true,
       },
       {
-        accessor: 'name',
-        Header: 'Customer Name',
+        accessor: "name",
+        Header: "Customer Name",
       },
       {
-        accessor: 'price',
-        Header: 'Price',
+        accessor: "price",
+        Header: "Price",
         formatter: MoneyFormatter,
       },
       {
-        accessor: 'address',
-        Header: 'Address',
+        accessor: "address",
+        Header: "Address",
       },
       {
-        accessor: 'phone',
-        Header: 'Phone',
+        accessor: "phone",
+        Header: "Phone",
       },
       {
-        accessor: 'quantity',
-        Header: 'Product Count',
+        accessor: "quantity",
+        Header: "Product Count",
       },
       {
-        accessor: 'status',
-        Header: 'Status',
+        accessor: "status",
+        Header: "Status",
         disableGlobalFilter: true,
         disableSortBy: true,
         formatter: StatusFormatter,
         width: 110,
       },
       {
-        Header: 'Action',
-        accessor: 'actionn',
+        Header: "Action",
+        accessor: "actionn",
         disableGlobalFilter: true,
         disableSortBy: true,
         formatter: ActionFormatter,
         width: 110,
-      }
-    ], [],
+      },
+    ],
+    []
   );
-
 
   const orderListTableData = { tableHeaderData: columns, tableRowsData: data };
   return orderListTableData;
 };
 
 // const ProductList = () => {
-  // const products = useSelector((state) => state);
-  // console.log(products);
+// const products = useSelector((state) => state);
+// console.log(products);
 
 // const fetchedProd = async () => {
-//   const res = await axios 
+//   const res = await axios
 //   .get('https://fakestoreapi.com/products')
 //   .catch((err) => {
 //     console.log(err);
--- src/redux/actions/loginActions.js
+++ src/redux/actions/loginActions.js
@@ -1,56 +1,54 @@
-import axios from 'axios';
-export const LOGGEDIN = 'LOGGEDIN';
-export const LOGIN_ERROR_AUTH = 'LOGIN_ERROR_AUTH';
-export const LOGIN_START  = 'LOGIN_START';
+import axios from "axios";
+export const LOGGEDIN = "LOGGEDIN";
+export const LOGIN_ERROR_AUTH = "LOGIN_ERROR_AUTH";
+export const LOGIN_START = "LOGIN_START";
 
 // start
 // sucess
 // fail
 
-const loginStart = ()=>{
-    return {
-        type:LOGIN_START
-    }
-}
- function loginchk( username, id, avatar ) {
+const loginStart = () => {
+  return {
+    type: LOGIN_START,
+  };
+};
+function loginchk(username, id, avatar) {
   return {
     type: LOGGEDIN,
     user: { username, id, avatar },
   };
 }
 
- function authError(error) {
+function authError(error) {
   return {
     type: LOGIN_ERROR_AUTH,
     error,
   };
 }
 
-export const login = (email,password) => dispatch=>{
- dispatch( loginStart());
+export const login = (email, password) => (dispatch) => {
+  dispatch(loginStart());
 
- axios.post('https://haatbazaar.herokuapp.com/api/v1/vendor/auth/signin', {
-    email: email,
-    password: password
-  })
-  .then(function (response) {
-    if(response.data.success === true){
-      var email = response.data.data.username;
-      var id = response.data.data._id;
-  dispatch(loginchk(email, id, 'ok'))
-  localStorage.setItem('token', response.data.token);
-  localStorage.setItem('loginsuccess', response.data.success)
-  return true;
-    }else{  
-   dispatch(authError('Username and password didnot matched'))
-  alert("Username and password didnot matched")
-  return false;
-    }
-  })
-  .catch(function (error) {
-    dispatch(authError(error.response.status))
-  alert(error.response.data.message)
-  return false;
-  });
-
-  }
+  axios
+    .post("https://haatbazaar.herokuapp.com/api/v1/vendor/auth/signin", {
+      email: email,
+      password: password,
+    })
+    .then(function (response) {
+      if (response.data.success === true) {
+        var email = response.data.data.username;
+        var id = response.data.data._id;
+        dispatch(loginchk(email, id, "ok"));
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("loginsuccess", response.data.success);
+        return true;
+      } else {
+        dispatch(authError("Username and password didnot matched"));
+        return false;
+      }
+    })
+    .catch(function (error) {
+      dispatch(authError(error.response.status));
+      return false;
+    });
+};
